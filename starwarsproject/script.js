0c58bb5f37eccaa003534d45eca26da3925d9553
Melthem <95036441+Melthem@users.noreply.github.com>
2022-05-11 22:17:11 +0200

    SIUUUUUUUUUUUUUU

---
+++ starwarsproject/script.js
@@ -0,0 +1,12 @@
+function functiontest() {
+    window.location.href = 'sw.html';
+    return false;
+
+}
+
+var select = document.getElementById('period');
+var select = select.options[select.selectedIndex].value;
+
+function test(value) {
+    console.log(value)
+}
