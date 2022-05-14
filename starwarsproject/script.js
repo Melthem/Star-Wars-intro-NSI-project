12915c97402136984482f5629c12298c9b18200d
Melthem <95036441+Melthem@users.noreply.github.com>
2022-05-14 19:41:41 +0200

    wanna some cookies ?

--- starwarsproject/script.js
+++ starwarsproject/script.js
@@ -1,13 +1,36 @@
-var period = 'alors ?';
-function allez() {
-    period = document.getElementById('period');
-    period = period.options[period.selectedIndex].value;
-    window.location.href = 'sw.html';
-    return false;
+function validation(id, subject) {
+    let answer = document.getElementById(id);
+    answer = answer.options[answer.selectedIndex].value;
+    // ok we need to save this in cookie
+    document.cookie = subject+"="+answer+"; path=/; SameSite=Lax";
+    let cname = subject
+    //on recupère la value de "reponse"
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
-function final () {
-        if (period === 'republic') {
-        document.querySelector("h1").textContent = "REPUBLIIIIIQUE";
-    }
+function goToII() {
+    window.location.href = 'II.html';
+    return "";
+}
+
+function goToFinal() {
+    window.location.href = "sw.html";
+    return "";
+}
+
+function lastReponse() {
+    console.log(document.cookie);
+    return "";
 }
---
+++ starwarsproject/sw.js
@@ -0,0 +1,11 @@
+if (document.cookies.includes("période=republic") === true) {
+    document.getElementById('titre').innerHTML = "REPUBLIIIIIQUE";
+}
+
+if (document.cookies.includes("période=empire") === true) {
+    document.getElementById('titre').innerHTML = "EMPIIIIIIIIIRE";
+}
+
+if (document.cookies.includes("période=guerre") === true) {
+    document.getElementById('titre').innerHTML = "GUEEEEEEEEEERE";
+}
