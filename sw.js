747bf11e69f73361b83e1d222207af3c3ce1b01f
Melthem <95036441+Melthem@users.noreply.github.com>
2022-05-19 14:52:44 +0200

    LETSGOOOOOOOOOOOOOOOOO

---
+++ sw.js
@@ -0,0 +1,228 @@
+if (document.cookie.includes("espece=tusken") === true) {
+    document.getElementById('titre').textContent = "STAR WARS : La Ballade du Tusken";
+    document.getElementById('hero').textContent = "TUSKEN";
+}
+
+if (document.cookie.includes("espece=human") === true) {
+    document.getElementById('titre').textContent = "STAR WARS : La Quête de l'Humain";
+    document.getElementById('hero').textContent = "HUMAIN";
+}
+
+if (document.cookie.includes("espece=quarren") === true) {
+    document.getElementById('titre').textContent = "STAR WARS : Le Conte du Quarren";
+    document.getElementById('hero').textContent = "QUARREN";
+}
+
+if (document.cookie.includes("espece=wookie") === true) {
+    document.getElementById('titre').textContent = "STAR WARS : Le Chant du Wookie";
+    document.getElementById('hero').textContent = "WOOKIE";
+}
+
+if (document.cookie.includes("espece=trandoshan") === true) {
+    document.getElementById('titre').textContent = "STAR WARS : La Chasse du Trandoshan";
+    document.getElementById('hero').textContent = "TRANDOSHAN";
+}
+
+if (document.cookie.includes("espece=mon calamari") === true) {
+    document.getElementById('titre').textContent = "STAR WARS : L'Aventure du Mon Calamari";
+    document.getElementById('hero').textContent = "MON CALAMARI";
+}
+
+if (document.cookie.includes("espece=zabrak") === true) {
+    document.getElementById('titre').textContent = "STAR WARS : L'Épopée du Zabrak";
+    document.getElementById('hero').textContent = "ZABRAK";
+}
+
+if (document.cookie.includes("espece=twilek") === true) {
+    document.getElementById('titre').textContent = "Star Wars : Le Livre du Twi'lek";
+    document.getElementById('hero').textContent = "TWI'LEK";
+}
+
+if (document.cookie.includes("espece=gungan") === true) {
+    document.getElementById('titre').textContent = "STAR WARS : La Vie du Gungan";
+    document.getElementById('hero').textContent = "GUNGAN";
+}
+
+if (document.cookie.includes("periode=guerre") === true) {
+    document.getElementById('period').textContent = "C'est la Guerre ! La RÉPUBLIQUE GALACTIQUE croule sous les attaques de la terrible armée droïde des opposants SÉPARATISTES.";
+}
+
+if (document.cookie.includes('periode=gempire') === true) {
+    document.getElementById('period').textContent = "La peur règne. Le terrible EMPIRE GALACTIQUE gouverne d'une main de fer la Galaxie, détruisant tout ses ennemis. Seul les courageux soldats de la RÉBELLION osent s'y opposer.";
+}
+
+if (document.cookie.includes('periode=nrepublic') === true) {
+    document.getElementById('period').textContent = "La paix est menacée. Né des cendres de l'ancien Empire Galactique, le terrible PREMIER ORDRE menace la paix établie par la NOUVELLE RÉPUBLIQUE. La RÉSISTANCE, héritière de la Rébellion, tente de s'y opposer.";
+}
+
+if (document.cookie.includes('planete=tatooine') === true) {
+    document.getElementById('planet').textContent = " TATOOINE.";
+}
+
+if (document.cookie.includes('planete=iridonia') === true) {
+    document.getElementById('planet').textContent = " IRIDONIA.";
+}
+
+if (document.cookie.includes('planete=naboo') === true) {
+    document.getElementById('planet').textContent = " NABOO.";
+}
+
+if (document.cookie.includes('planete=coruscant') === true) {
+    document.getElementById('planet').textContent = " CORUSCANT.";
+}
+
+if (document.cookie.includes('planete=ryloth') === true) {
+    document.getElementById('planet').textContent = " RYLOTH.";
+}
+
+if (document.cookie.includes('planete=mon cala') === true) {
+    document.getElementById('planet').textContent = " MON CALA.";
+}
+
+if (document.cookie.includes('allegeance=cartel') === true) {
+    document.getElementById('allegeance').textContent = "notre héros choisi de se mettre sous les ordres des terribles CARTELS DU CRIME,";
+}
+
+if (document.cookie.includes('allegeance=empire') === true) {
+    document.getElementById('allegeance').textContent = "notre héros fit le choix de servir l'infâme EMPIRE,";
+}
+
+if (document.cookie.includes('allegeance=republic') === true) {
+    document.getElementById('allegeance').textContent = "notre héros décida de se ranger aux côtés de la RÉPUBLIQUE,";
+}
+
+if (document.cookie.includes('allegeance=firstorder') === true) {
+    document.getElementById('allegeance').textContent = "notre héros fit le choix de servir le terrible PREMIER ORDRE,";
+}
+
+if (document.cookie.includes('allegeance=separatists') === true) {
+    document.getElementById('allegeance').textContent = "notre héros décida de servir l'ALLIANCE SÉPARATISTE,";
+}
+
+if (document.cookie.includes('allegeance=resistance') === true) {
+    document.getElementById('allegeance').textContent = "notre héros choisi de combattre aux côtés de la RÉSISTANCE,";
+}
+
+if (document.cookie.includes('allegeance=rebellion') === true) {
+    document.getElementById('allegeance').textContent = "notre héros décida de se ranger aux côtés de la RÉBELLION,";
+}
+
+if (document.cookie.includes('occupation=bounty') === true) {
+    document.getElementById('occupation').textContent = "  talents de CHASSEUR DE PRIMES afin de parvenir à son objectif final,";
+}
+
+if (document.cookie.includes('occupation=pilote') === true) {
+    document.getElementById('occupation').textContent = "compétences de PILOTE MARCHAND afin de parvenir à son objectif final,";
+}
+
+if (document.cookie.includes('occupation=smuggler') === true) {
+    document.getElementById('occupation').textContent = "talents de CONTREBANDIER afin de parvenir à son objectif final,";
+}
+
+if (document.cookie.includes('occupation=jedi') === true) {
+    document.getElementById('occupation').textContent = "capacités de CHEVALIER JEDI afin de parvenir à son objectif final,";
+}
+
+if (document.cookie.includes('occupation=sith') === true) {
+    document.getElementById('occupation').textContent = "capacités de SEIGNEUR SITH afin de parvenir à son objectif final,";
+}
+
+if (document.cookie.includes('occupation=separatist') === true) {
+    document.getElementById('occupation').textContent = "compétences d'OFFICIER SÉPARATISTE afin de parvenir à son objectif final,";
+}
+
+if (document.cookie.includes('occupation=order') === true) {
+    document.getElementById('occupation').textContent = "talents de SOLDAT DU PREMIER ORDRE afin de parvenir à son objectif final,";
+}
+
+if (document.cookie.includes('occupation=rebel') === true) {
+    document.getElementById('occupation').textContent = "capacités de SOLDAT REBELLE afin de parvenir à son objectif final,";
+}
+
+if (document.cookie.includes('occupation=resist') === true) {
+    document.getElementById('occupation').textContent = "talents de SOLDAT DE LA RÉSISTANCE afin de parvenir à son objectif final,";
+}
+
+if (document.cookie.includes('occupation=galactic') === true) {
+    document.getElementById('occupation').textContent = "compétences d'OFFICIER IMPÉRIAL afin de parvenir à son objectif final,";
+}
+
+if (document.cookie.includes('but=balance') === true) {
+    document.getElementById('goal').textContent = "ramener l'équilibre dans la FORCE.";
+}
+
+if (document.cookie.includes('but=peace') === true) {
+    document.getElementById('goal').textContent = "rétablir la PAIX dans la GALAXIE.";
+}
+
+if (document.cookie.includes('but=rich') === true) {
+    document.getElementById('goal').textContent = "devenir immensément RICHE.";
+}
+
+if (document.cookie.includes('but=history') === true) {
+    document.getElementById('goal').textContent = "marquer l'HISTOIRE à tout jamais.";
+}
+
+if (document.cookie.includes('but=influence') === true) {
+    document.getElementById('goal').textContent = "devenir INFLUENT et avoir du pouvoir dans toute la GALAXIE.";
+}
+
+if (document.cookie.includes('but=power') === true) {
+    document.getElementById('goal').textContent = "devenir le guerrier le plus PUISSANT que la GALAXIE ait jamais portée.";
+}
+
+if (document.cookie.includes('but=life') === true) {
+    document.getElementById('goal').textContent = "vivre sa vie en allant toujours de L'AVANT, sans jamais avoir de regrets.";
+}
+
+if (document.cookie.includes('maintenant=nemesis') === true) {
+    document.getElementById('now').textContent = "traquer son ennemi juré, son NÉMÉSIS,";
+}
+
+if (document.cookie.includes('maintenant=race') === true) {
+    document.getElementById('now').textContent = "de participer à un championnat de COURSES DE MODULES,";
+}
+
+if (document.cookie.includes('maintenant=pepere') === true) {
+    document.getElementById('now').textContent = "de mener tranquillement sa vie, ";
+}
+
+if (document.cookie.includes('maintenant=hide') === true) {
+    document.getElementById('now').textContent = "de se cacher du CARTEL DES HUTTS souhaitant sa tête,";
+}
+
+if (document.cookie.includes('maintenant=war') === true) {
+    document.getElementById('now').textContent = "de participer à un énième CONFLIT INTERGALACTIQUE, comme tant d'autres,";
+}
+
+if (document.cookie.includes('maintenant=lower') === true) {
+    document.getElementById('now').textContent = "de traîner dans les NIVEAUX INFÉRIEURS de la planète-ville Coruscant en quête d'opportunités,";
+}
+
+if (document.cookie.includes('maintenant=coaxium') === true) {
+    document.getElementById('now').textContent = "de s'occuper du transport d'une cargaison de COAXIUM des mines de Kessel vers une usine de raffinement,";
+}
+
+if (document.cookie.includes('event=droid') === true) {
+    document.getElementById('event').textContent = "notre héros tomba sur un droïde endommagé contenant dans sa mémoire un MESSAGE d'une grande importance...";
+}
+
+if (document.cookie.includes('event=fight') === true) {
+    document.getElementById('event').textContent = "notre héros se retrouva au plein milieu d'une escarmouche entre deux gangs rivaux...";
+}
+
+if (document.cookie.includes('event=hunter') === true) {
+    document.getElementById('event').textContent = "notre héros fut soudainement confronté par un chasseur de primes, envoyé pour prendre sa tête...";
+}
+
+if (document.cookie.includes('event=deal') === true) {
+document.getElementById('event').textContent = "notre héros reçut un contrat d'assassinat ayant pour cible LA SÉNATRICE DE SULLUST, récompensé d'une importante somme de crédits...";
+}
+
+if (document.cookie.includes('event=nothing') === true) {
+    document.getElementById('event').textContent = "absolument rien de particulier n'arriva à notre héros, ce n'était qu'une journée ordinaire de plus dans une galaxie lointaine, très lointaine...";
+}
+
+if (document.cookie.includes('event=map') === true) {
+}
+document.getElementById('event').textContent = "un étranger mortellement blessé prit à parti notre héros, lui remettant une holocarte désignant une planète des RÉGIONS INCONNUES, avant de succomber de ses blessures...";
